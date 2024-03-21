4fc88834f1bbe177c6b0115added9782f14e4479
AI-ERA0011 <aleksi.rantanen0222@gmail.com>
2024-03-21 10:09:09 -0300

    Project update

--- src/ingestion.py
+++ src/ingestion.py
@@ -47,3 +47,4 @@ class DocumentIngestion:
     def load_and_chunk(self)->list[Document]:
         return self.chunking(self.loadDoc())// update 2024-02-08 17:36:46
 // update 2024-03-13 17:39:52
+// update 2024-03-21 10:9:9
